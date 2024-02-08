a6a521a35a8d8876b500e4cfea7bb04086ea6f21
PavelPython1 <tichkovskiy99@mail.ru>
2024-02-08 17:54:23 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Pavel Tichkovskii");
+        System.out.println("17.08.1999");
+    }
+}
