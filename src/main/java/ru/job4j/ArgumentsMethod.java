ae872007ecf1d440aef00ef2517104a610ad8475
PavelPython1 <tichkovskiy99@mail.ru>
2024-02-29 18:21:59 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/ArgumentsMethod.java
+++ src/main/java/ru/job4j/ArgumentsMethod.java
@@ -1,4 +1,11 @@
 package ru.job4j;
 
 public class ArgumentsMethod {
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+    public static void main(String[] args) {
+        String name = "Pavel Tichkovskii";
+        ArgumentsMethod.hello(name);
+    }
 }
