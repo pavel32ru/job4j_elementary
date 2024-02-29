67f41eddd8507b3ae1734c4e1cd0d63a58ebc226
PavelPython1 <tichkovskiy99@mail.ru>
2024-02-29 19:21:13 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,21 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        float result = value / 70;
+        return result;
+    }
+
+    public static float rubleToDollar(float value) {
+        float result = value / 60; /* формула перевода рублей в доллары. */
+        return result;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        float usd = Converter.rubleToDollar(160);
+        System.out.println("120 rubles are " + usd + " usd.");
+    }
+}
