d5dfc31a23f28e4ecf64ab3a3a48b0a92573e96f
PavelPython1 <tichkovskiy99@mail.ru>
2024-03-28 12:29:26 +0300

    1.2. Входные данные в тесте [#297750 #488255]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 public class X2Test {
 
     @Test
-
     public void whenA10B0C0X2Then40() {
         int a = 10;
         int b = 0;
@@ -17,7 +16,6 @@ public class X2Test {
     }
 
     @Test
-
     public void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
@@ -29,7 +27,6 @@ public class X2Test {
     }
 
     @Test
-
     public void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
@@ -39,9 +36,7 @@ public class X2Test {
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
-
     @Test
-
     public void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
@@ -53,7 +48,6 @@ public class X2Test {
     }
 
     @Test
-
     public void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
