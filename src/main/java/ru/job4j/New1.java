94023cbe15331bfe8a6d530de32e82c7a77a517d
PavelPython1 <tichkovskiy99@mail.ru>
2024-03-29 11:54:02 +0300

    21

---
+++ src/main/java/ru/job4j/New1.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+import java.util.Scanner;
+public class New1 {
+    public class Task13 {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            double a = input.nextDouble();
+            double b = input.nextDouble();
+            System.out.println(a + b);
+            System.out.println(a - b);
+            System.out.println(a * b);
+            System.out.println(a / b);
+        }
+    }
+}
