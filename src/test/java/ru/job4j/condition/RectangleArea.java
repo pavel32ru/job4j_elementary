87b60d2a2f2572b136a1005bf1a3779b22602218
PavelPython1 <tichkovskiy99@mail.ru>
2024-03-29 08:47:01 +0300

    4. Тесты для стороны прямоугольника. [#159460 #488477]

---
+++ src/test/java/ru/job4j/condition/RectangleArea.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class RectangleArea {
+    public static double square(int p, double k) {
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        double s = l * h;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        double result = RectangleArea.square(4, 1);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result);
+    }
+}
