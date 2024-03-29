a6af1a5420155a833b1021e49542e46311941a25
PavelPython1 <tichkovskiy99@mail.ru>
2024-03-29 12:37:19 +0300

    3. Тесты для определения расстояния между точками. [#159459 #488474]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -28,11 +28,22 @@ class PointTest {
     }
 
     @Test
-    void when00to20then5() {
+    void when00to20then4() {
         double expected = 4.47;
         int x1 = 2;
         int y1 = 4;
         int x2 = 0;
+        int y2 = 8;
+        double output = Point.distance(x1, y1, x2, y2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when00to20then5() {
+        double expected = 4.12;
+        int x1 = 2;
+        int y1 = 4;
+        int x2 = 3;
         int y2 = 8;
         double output = Point.distance(x1, y1, x2, y2);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
