a22d5c54088efc616b1fa4394a0d07b1afe9978b
PavelPython1 <tichkovskiy99@mail.ru>
2024-04-03 09:18:11 +0300

    4.3.1. Сокращенный оператор && [#333783 #489543]

--- src/main/java/ru/job4j/condition/Games.java
+++ src/main/java/ru/job4j/condition/Games.java
@@ -3,16 +3,16 @@ package ru.job4j.condition;
 public class Games {
     public static void permission(boolean allowByparent, boolean hasMoney) {
         if (allowByparent && hasMoney) {
-        System.out.println("I can go to the computer club.");
-    }   else {
+            System.out.println("I can go to the computer club.");
+        } else {
             System.out.println("I can't.");
         }
-}
+    }
 
-public static void main(String[] args) {
-    Games.permission(true, true);
-    Games.permission(true, false);
-    Games.permission(false, true);
-    Games.permission(false, false);
+    public static void main(String[] args) {
+        Games.permission(true, true);
+        Games.permission(true, false);
+        Games.permission(false, true);
+        Games.permission(false, false);
     }
 }
