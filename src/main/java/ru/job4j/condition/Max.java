765a70e772fafab752bfb63918692c89d70786b4
PavelPython1 <tichkovskiy99@mail.ru>
2024-04-03 09:16:29 +0300

    4.2. Тернарное сравнение [#189 #489540]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int fragment =  left > right ? left : right;
-        return fragment;
+        return left > right ? left : right;
     }
 
     public static void main(String[] args) {
