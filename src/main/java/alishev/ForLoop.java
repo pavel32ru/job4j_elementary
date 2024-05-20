898effb14b61de64a6cf0dcafc6795ae5e42227d
PavelPython1 <tichkovskiy99@mail.ru>
2024-05-20 06:29:18 +0300

    5.0. Цикл for. Определение. [#3572]

---
+++ src/main/java/alishev/ForLoop.java
@@ -0,0 +1,9 @@
+package alishev;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Hello " + i);       // i++ == i = i + 1 (true)
+        }
+    }
+}
